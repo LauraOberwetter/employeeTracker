19db63236f1ecf3fb5205373303f8faf7bd1c41b
Laura Oberwetter <leoberwetter@gmail.com>
2021-04-25 23:38:57 -0500

    SQL logic and js responses

--- db/index.js
+++ db/index.js
@@ -6,10 +6,39 @@ class DB {
     constructor(connection){
         this.connection = connection
     }
-    
+    insertIntoDepartments() {
+        return this.connection.query(
+            "INSERT INTO departments (name) VALUES ()"
+        )   
+
+    }
+    insertIntoRoles() {
+        return this.connection.query(
+            "INSERT INTO roles (title, salary, department_id) VALUES ()"
+        )   
+
+    }
+    insertIntoEmployees() {
+        return this.connection.query(
+            "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ()"
+        )   
+
+    }
     selectAllEmployees() {
         return this.connection.query(
             "SELECT * from employee"
+        )   
+
+    }
+    selectAllRoles() {
+        return this.connection.query(
+            "SELECT * from role"
+        )   
+
+    }
+    selectAllDepartment() {
+        return this.connection.query(
+            "SELECT * from department"
         )   
 
     }
--- index.js
+++ index.js
@@ -5,11 +5,49 @@ require("console.table")
 
 console.log("app is running")
 
+//add departments // these functions need to change so we are seeing the database with the new addition
+async function addDepartment() {
+    let newDepartment = await db.insertIntoDepartments()
+    console.table(newDepartment)
+}
+addDepartment();
 
-//upon npm start, returns table of all employees
+//add roles
+async function addRoles() {
+    let newRole = await db.insertIntoRoles()
+    console.table(newRole)
+}
+addRoles();
+
+//add employees
+async function addEmployees() {
+    let newEmployee = await db.insertIntoEmployees()
+    console.table(newEmployee)
+}
+addEmployees();
+
+//view departments
+async function viewDepartment() {
+    let allDepartment = await db.selectAllDepartment()
+    console.table(allDepartment)
+}
+viewDepartment();
+
+//view roles
+async function viewRoles() {
+    let allRoles = await db.selectAllRoles()
+    console.table(allRoles)
+}
+viewRoles();
+
+
+//upon npm start, view all employees
 async function viewEmployees() {
     let allEmployees = await db.selectAllEmployees()
     console.table(allEmployees)
 }
-
 viewEmployees();
+
+//update employee roles
+
+//insert inquirer to build the interface
