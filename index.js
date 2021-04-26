f0f856fe266be2ec619cacbd5b800017083aac83
Laura Oberwetter <leoberwetter@gmail.com>
2021-04-26 08:04:25 -0500

    Inquirer prompts created

--- index.js
+++ index.js
@@ -1,44 +1,104 @@
-//import database connection
-const db = require("./db")
-//allows clean display of tabular data
-require("console.table")
+const db = require("./db") //import database connection
+require("console.table") //allows clean display of tabular data
+const inquirer = require('inquirer'); // require inquirer
 
-console.log("app is running")
+/////////////////////////start inquirer
+connection.connect((err) => {
+    if (err) throw err;
+    startApp();
+  });
+
+const startApp = () => {
+    inquirer
+      .prompt({
+        name: 'start',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'Add department',
+          'Add role',
+          'Add employee',
+          'View department',
+          'View role',
+          'View employee',
+          'Update employee role',
+          'exit',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.start) {
+          case 'Add department':
+            addDepartment();
+            break;
+  
+          case 'Add role':
+            addRoles();
+            break;
+  
+          case 'Add employee':
+            addEmployees();
+            break;
+  
+          case 'View department':
+            viewDepartment();
+            break;
+
+        case 'View role':
+            viewRoles();
+            break;
+        
+        case 'View employee':
+            viewEmployees();
+            break;
+        
+        case 'Update employee role':
+            updateEmployees();
+            break;
+  
+        case 'Exit':
+            connection.end();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+  };
+  ////////////////
 
 //add departments // these functions need to change so we are seeing the database with the new addition
 async function addDepartment() {
     let newDepartment = await db.insertIntoDepartments()
     console.table(newDepartment)
 }
-addDepartment();
 
 //add roles
 async function addRoles() {
     let newRole = await db.insertIntoRoles()
     console.table(newRole)
 }
-addRoles();
 
 //add employees
 async function addEmployees() {
     let newEmployee = await db.insertIntoEmployees()
     console.table(newEmployee)
 }
-addEmployees();
+
 
 //view departments
 async function viewDepartment() {
     let allDepartment = await db.selectAllDepartment()
     console.table(allDepartment)
 }
-viewDepartment();
+
 
 //view roles
 async function viewRoles() {
     let allRoles = await db.selectAllRoles()
     console.table(allRoles)
 }
-viewRoles();
+
 
 
 //upon npm start, view all employees
@@ -46,8 +106,13 @@ async function viewEmployees() {
     let allEmployees = await db.selectAllEmployees()
     console.table(allEmployees)
 }
-viewEmployees();
+
 
 //update employee roles
+
+async function updateEmployees() {
+    let updateEmployees = await db.selectAllEmployees()
+    console.table(allEmployees)
+}
 
 //insert inquirer to build the interface
